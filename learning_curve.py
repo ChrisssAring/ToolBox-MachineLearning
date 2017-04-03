1e9a383004d5db1edc7e22ecce4101f9e73c826f
Chris Aring <christopher.aring@students.olin.edu>
2017-04-03 17:24:34 -0400

    Toolbox Done!

--- learning_curve.py
+++ learning_curve.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy
+import math
 from sklearn.datasets import *
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -21,7 +22,7 @@ def display_digits():
 
 def train_model():
     """Train a model on pictures of digits.
-    
+
     Read in 8x8 pictures of numbers and evaluate the accuracy of the model
     when different percentages of the data are used as training data. This function
     plots the average accuracy of the model as a function of the percent of data
@@ -31,15 +32,18 @@ def train_model():
     num_trials = 10
     train_percentages = range(5, 95, 5)
     test_accuracies = numpy.zeros(len(train_percentages))
+    model = LogisticRegression(C=10**-10)
 
-    # train models with training percentages between 5 and 90 (see
-    # train_percentages) and evaluate the resultant accuracy for each.
-    # You should repeat each training percentage num_trials times to smooth out
-    # variability.
-    # For consistency with the previous example use
-    # model = LogisticRegression(C=10**-10) for your learner
+    for percentage in range(len(train_percentages)):
+        test_avg = []
+        for i in range(num_trials):
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_percentages[percentage]/100)
+            model.fit(X_train, y_train)
+            test_avg.append(model.score(X_test, y_test))
+        test_accuracies[percentage] = numpy.mean(test_avg)
 
-    # TODO: your code here
+    print("Train accuracy %f" %model.score(X_train, y_train))
+    print("Test accuracy %f"%model.score(X_test, y_test))
 
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
@@ -50,5 +54,5 @@ def train_model():
 
 if __name__ == "__main__":
     # Feel free to comment/uncomment as needed
-    display_digits()
-    # train_model()
+    #display_digits()
+    train_model()
